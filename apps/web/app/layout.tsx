391bfb74cb4940db85f0a77869d482d0be37eb53
Milan <milanthegamer@gmail.com>
2023-10-10 00:08:12 +0400

    shadcn module v0.1 complete

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,18 +1,19 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next'
+import 'ui/styles/globals.css'
 
 export const metadata: Metadata = {
-  title: "GEM App",
-  description: "Best Decentralised Lottery Platform",
-};
+  title: 'GEM App',
+  description: 'Best Decentralised Lottery Platform',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }): JSX.Element {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
